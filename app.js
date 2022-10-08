f54756e5f95cd8230641025e070921ce6b0c4063
elisamevangelista <elisamevangelista@yahoo.com.ar>
2022-10-08 00:06:00 -0300

    Pendiente terminar C06  formularios

--- app.js
+++ app.js
@@ -8,4 +8,16 @@ app.get("/", (req, res)=>{
     res.sendFile(path.join(__dirname, "./views/home.html"))
 })
 
+app.get("/home", (req, res)=>{
+    res.sendFile(path.join(__dirname, "./views/home.html"))
+})
+
+app.get("/login", (req, res)=>{
+    res.sendFile(path.join(__dirname, "./views/login.html"))
+})
+
+app.get("/register", (req, res)=>{
+    res.sendFile(path.join(__dirname, "./views/register.html"))
+})
+
 app.use(express.static(path.join(__dirname, "public")))
