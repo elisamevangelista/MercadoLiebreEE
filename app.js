a58196e5d57d9a76221e9269b0b23b94f50410b3
elisamevangelista <elisamevangelista@yahoo.com.ar>
2022-10-12 00:03:06 -0300

    Trabajo completado

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ app.get("/home", (req, res)=>{
     res.sendFile(path.join(__dirname, "./views/home.html"))
 })
 
+
 app.get("/login", (req, res)=>{
     res.sendFile(path.join(__dirname, "./views/login.html"))
 })
